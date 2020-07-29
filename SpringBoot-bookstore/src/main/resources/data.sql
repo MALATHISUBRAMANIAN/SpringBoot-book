b144968f88f05b1febf227f38a51e5366f64c8c5
MALATHISUBRAMANIAN <30365873+MALATHISUBRAMANIAN@users.noreply.github.com>
2020-07-29 16:16:45 +0530

    Update data.sql

--- SpringBoot-bookstore/src/main/resources/data.sql
+++ SpringBoot-bookstore/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO books (bookid,bookname,author,price,year) VALUES
-  (125, 'Harry Potter', 'J K Rowling',345,2002),
-  (235, 'Ulysses', 'Stephen King',299,1998),
-  (236, 'The Great Gatsby', 'David Foster Wallace',399,2015),
-  (237, 'Marcel Proust', 'Marcel Proust',655,2010),
-  (238, 'William Faulkner', 'William Faulkner',599,2020);
+INSERT INTO books (bookid,bookname,author,year,price) VALUES
+  (125, 'Harry Potter', 'J K Rowling',1995,222),
+  (235, 'Ulysses', 'Stephen King',1998,5000),
+  (236, 'The Great Gatsby', 'David Foster Wallace',2015,699),
+  (237, 'Marcel Proust', 'Marcel Proust',2010,345),
+  (238, 'William Faulkner', 'William Faulkner',2020,200);
   
